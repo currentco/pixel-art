a4901ddf0f6538f212891d2e259b1e299ff53896
EV <Eevamaija@Eevamaija-MacBook-Pro.local>
2018-02-01 22:10:09 +0200

    Version 2.0

--- designs.js
+++ designs.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
 function makeGrid() {
+
   // selects input values for the grid
   let rows = $('#inputHeight').val();
   let columns = $('#inputWidth').val();
   let table = $('#pixelCanvas');
+  
   // removes any previous child elements of the table 
   table.children().remove();
+  
   // creates the grid according to the input values
   for(var x = 0; x < rows; x++) {
     table.append('<tr></tr>');
@@ -13,6 +16,7 @@ function makeGrid() {
       table.children().last().append('<td></td>');
     }
   }
+  
 // after clicking on a cell, adds selected color
 $(table).on('click', 'td', function () {
 let color = $('#colorPicker').val();
@@ -24,10 +28,18 @@ $('td').contextmenu(function(e) {
 e.preventDefault();
 $(this).css('background-color', '');
 });
+
+// clicking the button clears the grid
+$('#clear').click(function() {
+$('td').css('background-color', '');
+});
 }
+
 // monitors the submit button to call the makeGrid function
 $('#submit-button').click(function(z) {
   z.preventDefault();
   makeGrid();
+  $('p').css('color', 'black');
+  $('#clear').css('visibility', 'visible');
 });
 });
